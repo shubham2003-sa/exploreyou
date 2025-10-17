6eaf6e45e8e0311a93d233b33fa8ae5d84efc05c
Shubham Agarwal <22b2158@iitb.ac.in>
2025-10-17 22:38:40 +0530

    edit2

--- my-app/postcss.config.mjs
+++ my-app/postcss.config.mjs
@@ -1,9 +1,9 @@
-import tailwindcss from "@tailwindcss/postcss"
-import autoprefixer from "autoprefixer"
-
 /** @type {import('postcss-load-config').Config} */
 const config = {
-  plugins: [tailwindcss, autoprefixer],
+  plugins: {
+    "@tailwindcss/postcss": {},
+    autoprefixer: {},
+  },
 }
 
 export default config
